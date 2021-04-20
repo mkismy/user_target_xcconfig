a88773cf8152fae48c31e080322d1374217c95f3
Yuta Amakawa <amakawa@jxpress.net>
2021-04-20 20:09:22 +0900

    Initial Commit

---
+++ user_target_xcconfig/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  user_target_xcconfig
+//
+//  Created by Yuta Amakawa on 2021/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
